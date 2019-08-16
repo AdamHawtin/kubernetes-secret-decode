ac456c523b25e9697e3de432ee5f3ea9c84bdf43
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-08-16 10:53:30 +0100

    Add more unit tests

--- test/test_decode_kubernetes_secret.py
+++ test/test_decode_kubernetes_secret.py
@@ -1,7 +1,10 @@
 import base64
 import copy
+import json
 
-from decode_kubernetes_secret import decode_data_fields, guess_secret_format
+import yaml
+
+import decode_kubernetes_secret as under_test
 
 
 def test_decode_data_fields():
@@ -18,7 +21,7 @@ def test_decode_data_fields():
     secret['data']['key2'] = base64.b64encode(secret['data']['key2'].encode())
 
     # When
-    actual_decoded_secret = decode_data_fields(secret)
+    actual_decoded_secret = under_test.decode_data_fields(secret)
 
     # Then
     assert expected_decoded_secret == actual_decoded_secret
@@ -29,7 +32,7 @@ def test_guess_secret_format_json():
     secret = '{"thisLooksLike": "JSON"}'
 
     # When
-    secret_format = guess_secret_format(secret)
+    secret_format = under_test.guess_secret_format(secret)
 
     # Then
     assert 'json' == secret_format
@@ -41,7 +44,53 @@ def test_guess_secret_format_yaml():
               'thisLooksLike: YAML')
 
     # When
-    secret_format = guess_secret_format(secret)
+    secret_format = under_test.guess_secret_format(secret)
 
     # Then
     assert 'yaml' == secret_format
+
+
+def test_json_decode_data_fields():
+    # Given
+    expected_decoded_secret = {
+        'data': {
+            'key1': 'value1',
+            'key2': 'value2',
+        },
+        'metadata': 'stuff'
+    }
+
+    secret = copy.deepcopy(expected_decoded_secret)
+    secret['data']['key1'] = base64.b64encode(secret['data']['key1'].encode()).decode()
+    secret['data']['key2'] = base64.b64encode(secret['data']['key2'].encode()).decode()
+    secret_json = json.dumps(secret)
+
+    # When
+    actual_decoded_secret_json = under_test.json_decode_data_fields(secret_json)
+    actual_decoded_secret = json.loads(actual_decoded_secret_json)
+
+    # Then
+    assert expected_decoded_secret == actual_decoded_secret
+
+
+def test_yaml_decode_data_fields():
+    # Given
+    expected_decoded_secret = {
+        'data': {
+            'key1': 'value1',
+            'key2': 'value2',
+        },
+        'metadata': 'stuff'
+    }
+
+    secret = copy.deepcopy(expected_decoded_secret)
+    secret['data']['key1'] = base64.b64encode(secret['data']['key1'].encode()).decode()
+    secret['data']['key2'] = base64.b64encode(secret['data']['key2'].encode()).decode()
+    secret_yaml = yaml.safe_dump(secret)
+
+    # When
+    actual_decoded_secret_json = under_test.yaml_decode_data_fields(secret_yaml)
+    actual_decoded_secret = yaml.safe_load(actual_decoded_secret_json)
+
+    # Then
+    assert expected_decoded_secret == actual_decoded_secret
