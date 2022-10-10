0780c55caa199a55477d6e62e1e5ded324d70e19
AdamHawtin <adam.hawtin@ons.gov.uk>
2022-10-10 08:13:21 +0100

    Fix setup conflict issue, improve optional arg behaviour

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='ksd',
-    version='0.2.0',
+    version='0.2.1',
     entry_points={
         'console_scripts': [
             'ksd=main:main'
         ]
     },
+    py_modules=[],
     python_requires=">=3.10",
 )
